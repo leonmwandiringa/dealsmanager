3b4e98f965cbe9277dd27d9c4831ff68a1d2a94a
Leon Mwandiringa <tinashe.leon@yahoo.com>
2018-01-23 12:37:55 +0200

    auth still

---
+++ Public/Assets/js/login.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    "use strict";
+    $("#loginbtn").click(loginUser);
+    var email, token;
+    var alert = $("#signinalert");
+
+    function loginUser(){
+
+        email = $("#hdemailaddr").val();
+        password = $("#password").val();
+        token = $("#hdtokenaddr").val();
+
+        if(password != ""){
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST","http://localhost/dealsmanager/login",true);
+            xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
+            xhr.onload = function(){
+
+                if(this.status == 200){
+
+                    console.log(this.responseText);
+
+                }
+
+            }
+            xhr.send("email="+email+"&token="+token);
+        }else{
+
+            alert.fadeIn(500);
+            alert.find("#alertnotice").text("Error");
+            alert.find("#alertmessage").text("Please fill in your Your OTP");
+
+        }
+
+    }
+
+});
